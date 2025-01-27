89005bf91d015476e72a8a66e345374b9032e2b8
Lucas Machado <lucasemachado.oficial@gmail.com>
2025-01-26 21:43:45 -0300

    Inserido novas classes e terminado o JS

--- src/js/index.js
+++ src/js/index.js
@@ -15,6 +15,7 @@ anterior da lista
     - passo 4 - buscar o cartão que esta selecionado e esconder
 */
 
+
 /*
 OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
 cartao da lista
@@ -26,6 +27,24 @@ const botaoAvancar = document.getElementById("btn-avancar");
 const cartoes = document.querySelectorAll(".cartao");
 
 let cartaoAtual = 0;
+
+
+cartoes.forEach(cartao =>{
+  cartao.addEventListener("click", function(){
+    const cartaVirada = cartao.querySelector(".carta-virada");
+
+    // Virar a carta - Para isso usamos o toggle. Se o elemento tiver a classe, ele vai remover. Se o elemento não tiver, ele adiciona.
+    cartao.classList.toggle("virar");
+    //  Mostrar o fundo da carta virada
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+    //Aqui está escondendo a descrição da carta para quando virar a carta, ela ocupar corretamente o espaço
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+
+  });
+});
+
 
 /*
 OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
